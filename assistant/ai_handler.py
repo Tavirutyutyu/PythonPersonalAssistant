c428db55f490ea7a29ec43f3bf6340941b001fa0
tavirutyutyu <erdosmarci@gmail.com>
2025-04-16 10:17:47 +0200

    add ai handler and gpt handler

---
+++ assistant/ai_handler.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+
+class AIHandler(ABC):
+    def __init__(self, model:str):
+        self._model = model
+        self._message_history = []
+    @abstractmethod
+    def generate_response(self, prompt:str):
+        pass
--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 import os
 
 RESOURCES_DIR = os.path.join(os.path.dirname(__file__), "resources")
+GPT_MODEL = "gpt-3.5-turbo"
