949eb499151208361a6f33360cdcc395ebbbfe9e
tavirutyutyu <erdosmarci@gmail.com>
2025-04-16 16:21:57 +0200

    create exit command

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -1,6 +1,9 @@
+import sys
+
 from assistant.ai_handler import AIHandler, OllamaHandler
 from assistant.ai_manager import LocalAIManager, OllamaManager
 from commands import CommandManager
+from commands.command_base import Command
 from voice import VoiceAssistant
 
 
@@ -23,26 +26,35 @@ class Assistant:
             command = self.command_manager.match(voice_input)
             if command:
                 self.voice_assistant.speak(f"You choose {command}")
-                self.voice_assistant.speak("Choose an option:")
-                options = command.get_sub_option_keys()
-                for option in options:
-                    self.voice_assistant.speak(option)
-                voice_option_input = self.voice_assistant.listen()
-                voice_option_input = voice_option_input.lower() if voice_option_input else None
-
-                if voice_option_input and voice_option_input in options:
-                    command.execute(voice_option_input)
-                elif voice_option_input not in options:
-                    print(f"You choose this option: {voice_option_input}")
-                    self.voice_assistant.speak("Invalid option")
-                elif not voice_option_input:
-                    self.voice_assistant.speak("Could not hear you.")
+                if command.has_sub_options:
+                    self.__execute_complex_command(command)
+                else:
+                    command.execute()
             else:
                 self.voice_assistant.speak("Generating answer with AI.")
                 ai_answer = self.ai_handler.generate_response(voice_input)
                 self.voice_assistant.speak(ai_answer)
 
+    def __choose_option(self, options: list[str]):
+        for option in options:
+            self.voice_assistant.speak(option)
+        voice_option_input = self.voice_assistant.listen()
+        return voice_option_input.lower()
+
+    def __execute_complex_command(self, command: Command):
+        self.voice_assistant.speak("Choose an option:")
+        options = command.get_sub_option_keys()
+        voice_option_input = self.__choose_option(options)
+        if voice_option_input and voice_option_input in options:
+            command.execute(voice_option_input)
+        elif voice_option_input not in options:
+            print(f"You choose this option: {voice_option_input}")
+            self.voice_assistant.speak("Invalid option")
+        elif not voice_option_input:
+            self.voice_assistant.speak("Could not hear you.")
+
     def shutdown(self):
         self.voice_assistant.speak("Shutting down the assistant.")
         self.voice_assistant.speak("Good bye!")
         self.local_ai_manager.stop_server()
+        sys.exit()
--- commands/commands.py
+++ commands/commands.py
@@ -1,4 +1,5 @@
 import subprocess
+import sys
 import webbrowser
 
 from commands.command_base import Command
@@ -8,7 +9,7 @@ class HelloCommand(Command):
     def __init__(self):
         super().__init__(name="hello command", file_name="hello_command.json")
 
-    def execute(self, name: str) -> str:
+    def execute(self, name: str | None = None) -> str:
         return f"Hello {name}! How can i help you?"
 
 
@@ -17,7 +18,7 @@ class BrowserCommand(Command):
     def __init__(self):
         super().__init__(name="browser command", file_name="browser_command.json")
 
-    def execute(self, text: str):
+    def execute(self, text: str | None = None):
         sub_options = self.get_sub_options()
         url = sub_options.get(text.lower(), sub_options["default"])
         webbrowser.open(url)
@@ -27,7 +28,7 @@ class LaunchIDECommand(Command):
     def __init__(self):
         super().__init__(name="code editor command", file_name="launch_ide_command.json")
 
-    def execute(self, text: str):
+    def execute(self, text: str | None = None):
         text = text.lower()
         try:
             sub_options = self.get_sub_options()
@@ -37,3 +38,11 @@ class LaunchIDECommand(Command):
             print(f"IDE not found: {e}")
         except Exception as e:
             print(f"{e.__class__.__name__} error: {e}")
+
+
+class ExitCommand(Command):
+    def __init__(self):
+        super().__init__(name="exit command", file_name="exit_command.json")
+
+    def execute(self, text: str | None = None):
+        sys.exit()
